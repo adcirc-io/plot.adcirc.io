ff284ed6efd5b2a7cd1462f2a4b991d5f82f5e20
Tristan <atdyer@ncsu.edu>
2016-06-13 14:04:06 -0400

    Add hover effects to plots

--- js/plotter.js
+++ js/plotter.js
@@ -4,39 +4,91 @@ function Plotter () {
     // Scoping
     var self = this;
 
-    this.active_plot = new Plot( '#plotting-area' );
-    this.inactive_plots = [];
-    
-    // Data Manager
+    // Variables
+    this.active_plot = null;
     this.data_manager = new DataManager();
-
-    // Sidebar
+    this.inactive_plots = [];
     this.sidebar = new Sidebar( 'sidebar' );
 
     // Page elements
-    this.add_plot = $( '#plot-placeholder' )[0];
-    this.fort_63_picker = $( '#fort63_picker' )[0];
+    this.button_add_plot = $( '#plot-placeholder' )[0];
+    this.picker_fort_63 = $( '#fort63_picker' )[0];
 
+    
+    this.add_plot = function () {
+        
+        // Create the plot
+        var plot = new Plot( '#plotting-area' );
+        var element = $( '#' + plot.id )[0];
+
+        element.addEventListener( 'click', self.on_click );
+        element.addEventListener( 'mouseenter', self.on_mouse_enter );
+        element.addEventListener( 'mouseleave', self.on_mouse_leave );
+        
+        return plot;
+        
+    };
+
+    this.initialize = function () {
+
+        self.active_plot = self.add_plot();
+
+        // Event listeners
+        self.data_manager.addEventListener( 'new_data', self.on_new_data );
+        self.button_add_plot.addEventListener( 'click', self.on_add_plot );
+        self.picker_fort_63.addEventListener( 'change', self.on_fort_63_picked );
+
+    };
+    
+    
     // Event handlers
     this.on_add_plot = function () {
 
-        self.inactive_plots.push( new Plot( '#plotting-area' ) );
+        var plot = self.add_plot();
+        
+        self.inactive_plots.push( plot );
+
+    };
+
+    this.on_click = function ( event ) {
+
+        // The parent level plot is bound to this when the event is fired
+        var plot = $( '#' + this.id );
+
+        plot.removeClass( 'plot-hover' );
+        plot.addClass( 'plot-active' );
 
     };
 
     this.on_fort_63_picked = function () {
 
         // Get the file
-        var file = self.fort_63_picker.files[0];
+        var file = self.picker_fort_63.files[0];
 
         // Tell data manager to open it
         self.data_manager.open_fort_63( file );
 
         // Reset the picker
-        self.fort_63_picker.value = null;
+        self.picker_fort_63.value = null;
 
     };
 
+    this.on_mouse_enter = function ( event ) {
+        
+        var plot = $( '#' + event.target.id );
+
+        plot.addClass( 'plot-hover' );
+        
+    };
+
+    this.on_mouse_leave = function ( event ) {
+
+        var plot = $( '#' + event.target.id );
+
+        plot.removeClass( 'plot-hover' );
+
+    };
+    
     this.on_new_data = function ( event ) {
 
         // Get the dataset
@@ -70,11 +122,8 @@ function Plotter () {
 
     };
 
-    // Event listeners
-    self.data_manager.addEventListener( 'new_data', self.on_new_data );
 
-    self.add_plot.addEventListener( 'click', self.on_add_plot );
-    self.fort_63_picker.addEventListener( 'change', self.on_fort_63_picked );
+    self.initialize();
 
 }
 
